803f10a663dfae76d4d19012857f0a242668928b
Adam Pengilly <adampengilly@Adams-MacBook-Pro.local>
2021-02-02 15:25:41 +0000

    updated code

--- traffax.py
+++ traffax.py
@@ -124,7 +124,7 @@ ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 ax.axis('off')
 ax = plt.scatter(london['Longitude'], london['Latitude'], s =4, color='blue')
-ax = plt.scatter(temp['Longitude'], temp['Latitude'], s =1.5, color='orange')
+#ax = plt.scatter(temp['Longitude'], temp['Latitude'], s =1.5, color='orange')
 
 st.pyplot(fig)
 
