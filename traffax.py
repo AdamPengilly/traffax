9938c76c5434397967e7d51181712399e771b5bd
AdamPengilly <44409387+AdamPengilly@users.noreply.github.com>
2021-02-02 13:55:51 +0000

    Add files via upload

--- traffax.py
+++ traffax.py
@@ -20,7 +20,7 @@ import json
 import datetime
 import pickle
 
-#CLOAD IN DATA
+#LOAD IN DATA
     
 london = pickle.load(open('london_slim_pickle', 'rb'))
 
@@ -29,12 +29,50 @@ def trans_pick_short():
     #Heat map won't work if there is a small amount of data. Use this list of the top occuring veh_types
     transport_pick_short = list(london.Veh_Type_Grouped.value_counts().sort_values(ascending=False)[:20].index)
     transport_pick_short.insert(0,'ALL') #insert to start of list
+    transport_pick_short.insert(1,'Pedal cycle (ALL)')
+    transport_pick_short.insert(2,'Car (ALL)')
+    transport_pick_short.insert(3,'Motorcycle (ALL)')
+    transport_pick_short.insert(4,'Bus/Coach (ALL)')
+    transport_pick_short.insert(5,'LGV/HGV (ALL)')
     return tuple(transport_pick_short)
 transport_pick_short = trans_pick_short()
 
+#Function that plots accident locations by vehicle type over London (for entire time period)
+def acc_by_vehtype_map(veh_types='Car, Car', fig_size=12):
+    %matplotlib inline
+    fig = plt.figure(figsize=(fig_size,fig_size))
+    ax = fig.add_subplot()    
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    ax.axis('off')
+    #Met Police & City of London Police - BASE LAYER (all accidents)   
+    ax = plt.scatter(london['Longitude'], london['Latitude'], s =4)
+    if veh_types == 'ALL':
+        temp = london
+        
+    elif veh_types == 'Pedal cycle (ALL)':
+        temp = london[london.Veh_Type_Grouped.str.contains('Pedal cycle')]
+        
+    elif veh_types == 'Car (ALL)':
+        temp = london[london.Veh_Type_Grouped.str.contains('Car')]
+
+    elif veh_types == 'Motorcycle (ALL)':
+        temp = london[london.Veh_Type_Grouped.str.contains('Motorcycle')]
+    
+    elif veh_types == 'Bus/Coach (ALL)':
+        temp = london[london.Veh_Type_Grouped.str.contains('Bus')]
+        
+    elif veh_types == 'LGV/HGV (ALL)':
+        temp = london[london.Veh_Type_Grouped.str.contains('LGV') | london.Veh_Type_Grouped.str.contains('HGV')]
+    
+    else:
+        temp = london[london.Veh_Type_Grouped == veh_types]
+        
+    ax = plt.scatter(temp['Longitude'], temp['Latitude'], s =1.5, color='orange')
 
 
-#Function that plots accidents by vehicle type over london
+
+#Function that plots accidents by vehicle type over london over time
 def heatmap_ts(veh_types='ALL', map_type='OpenStreetMap'):  
     
     time_index = []
@@ -45,6 +83,22 @@ def heatmap_ts(veh_types='ALL', map_type='OpenStreetMap'):
             time_index.append(idx)
             if veh_types == 'ALL':
                 temp = london
+                
+            elif veh_types == 'Pedal cycle (ALL)':
+                temp = london[london.Veh_Type_Grouped.str.contains('Pedal cycle')]
+
+            elif veh_types == 'Car (ALL)':
+                temp = london[london.Veh_Type_Grouped.str.contains('Car')]
+
+            elif veh_types == 'Motorcycle (ALL)':
+                temp = london[london.Veh_Type_Grouped.str.contains('Motorcycle')]
+
+            elif veh_types == 'Bus/Coach (ALL)':
+                temp = london[london.Veh_Type_Grouped.str.contains('Bus')]
+
+            elif veh_types == 'LGV/HGV (ALL)':
+                temp = london[london.Veh_Type_Grouped.str.contains('LGV') | london.Veh_Type_Grouped.str.contains('HGV')]
+                
             else:
                 temp = london[london.Veh_Type_Grouped == veh_types]
                 
@@ -70,14 +124,19 @@ st.title("London Traffic Accident Map (2005-15)")
 #st.header("header")
 #st.subheader("subheader")
 
+
 veh_select = st.selectbox('Vehicle Combination', transport_pick_short)
 map_select = st.selectbox('Map Style', ('OpenStreetMap', 'Stamen Terrain', 'Stamen Toner', 'CartoDB Dark_Matter'))
 
 # other maps include 'Thunderforest.Neighbourhood', 'CyclOSM', 'Stamen.Watercolor', 
 # BUT as these are custom... need to add 'attr' parameter to give credit
 
+#LONDON ACCIDENT PLOT
+acc_by_vehtype_map(veh_types=veh_select,)
 
-folium_static(heatmap_ts(veh_types=veh_select, map_type=map_select), width=550, height=400)
+
+#HEATMAP
+folium_static(heatmap_ts(veh_types=veh_select, map_type=map_select), width=650, height=600)
 
 
 
